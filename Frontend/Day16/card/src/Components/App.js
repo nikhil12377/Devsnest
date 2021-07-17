68f63dc012980638cfc1f376da34a26f009454fc
nikhil12377 <48783530+nikhil12377@users.noreply.github.com>
2021-07-17 12:59:54 +0530

    Add files via upload

---
+++ Frontend/Day16/card/src/Components/App.js
@@ -0,0 +1,13 @@
+import Card from './Card'
+import "./Style.css";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Card />
+    </div>
+  );
+}
+
+export default App;
---
+++ Frontend/Day16/card/src/Components/Card.jsx
@@ -0,0 +1,15 @@
+import "./Style.css";
+function card() {
+  return (
+    <div className="card">
+      <img
+        src="https://i.insider.com/5df773d8fd9db276fe0a6c55?width=1136&format=jpeg"
+        alt=""
+      />
+      <h1>React Cards</h1>
+      <p>this is a card</p>
+    </div>
+  );
+}
+
+export default card;
