197b080f116ab1cd71e06587ec93e3c747048a71
nikhil12377 <nikhild12377@gmail.com>
2021-09-23 17:11:34 +0530

    Added Day 9

--- Backend/Day9/index.js
+++ Backend/Day9/index.js
@@ -3,10 +3,19 @@ const app = express();
 const path = require('path');
 
 app.set("views", path.join(__dirname, "views"));
-app.set("view engine", 'jade');
-app.use('/download', (req, res) => {
-    // res.sendFile(path.join(__dirname, "public/hello.txt"), "test.txt");
+app.set("view engine", "jade");
+app.use('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "public/hello.txt"), "test.txt");
     res.download(path.join(__dirname, "public/hello.txt"), "ello.txt")
+    res.render('index', { title: "Express" })
+    res
+        .status(201)
+        .cookie("token", "test", {
+            expires: new Date(Date.now() + 8 * 9600)
+        })
+        .cookie("hello", "hello")
+        .redirect(404, "/admin")
 });
+
 
 app.listen(5000);
